0d700b3bee39c1642ecde8eef30404a875c52a5e
Tanawat Pinthongpan <tanawat.works@gmail.com>
2023-11-29 21:35:44 +0700

    feat: responsive logo

--- src/components/CoreLayout/Sidebar/Content.tsx
+++ src/components/CoreLayout/Sidebar/Content.tsx
@@ -6,7 +6,7 @@ import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useSelector } from 'react-redux'
-import { styled } from 'twin.macro'
+import tw, { styled } from 'twin.macro'
 
 import Logo from '@/assets/images/logo/logo-crop.png'
 import LogoMini from '@/assets/images/logo/logo-mini.png'
@@ -16,26 +16,29 @@ export type ContentProps = {
   className?: string
 }
 
+const FullLogo = () => <Image src={Logo} width={170} alt='Logo' />
+const MiniLogo = () => <Image src={LogoMini} width={55} alt='Logo' className='ml-[theme(spacing.1)-1px]' />
+
 export const Content: React.FC<ContentProps> = ({ className }) => {
   const { sidebarType } = useSelector(appSettingSelector)
 
   return (
     <div className={clsx('transition-width', className)}>
       {sidebarType !== 'mini' && (
-        <div className='flex items-center justify-center h-[--navbar-h]'>
-          <Image src={Logo} width={170} alt='Logo' />
-        </div>
+        <StyledLogoWrap>
+          <FullLogo />
+        </StyledLogoWrap>
       )}
 
       {sidebarType === 'mini' && (
         <>
-          <div id='logo' className='hidden items-center justify-center h-[--navbar-h]'>
-            <Image src={Logo} width={170} alt='Logo' />
-          </div>
+          <StyledLogoWrap id='logo' hidden>
+            <FullLogo />
+          </StyledLogoWrap>
 
-          <div id='logo-mini' className='flex items-center justify-center h-[--navbar-h]'>
-            <Image src={LogoMini} width={55} alt='Logo' className='ml-[theme(spacing.1)-1px]' />
-          </div>
+          <StyledLogoWrap id='logo-mini'>
+            <MiniLogo />
+          </StyledLogoWrap>
         </>
       )}
 
@@ -67,4 +70,8 @@ export const Content: React.FC<ContentProps> = ({ className }) => {
 
 const StyledContent = styled.div<{ sidebarType: SidebarType }>(({ sidebarType }) => {
   return []
+})
+
+const StyledLogoWrap = styled.div<{ hidden?: boolean }>(({ hidden }) => {
+  return [tw`items-center justify-center h-[--navbar-h]`, hidden ? tw`hidden` : tw`flex`]
 })
