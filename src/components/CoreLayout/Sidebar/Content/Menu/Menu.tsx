8f1c6aa58079c3c07e897f257986790dec3409c9
Tanawat.P <tanawat.p@appman.co.th>
2023-12-03 20:51:46 +0700

    feat: refactor code a little bit 3

--- src/components/CoreLayout/Sidebar/Content/Menu/Menu.tsx
+++ src/components/CoreLayout/Sidebar/Content/Menu/Menu.tsx
@@ -3,10 +3,9 @@ import type { IconType } from '@/components/Icon'
 import type { KeyboardEvent } from 'react'
 import { useRef, useState } from 'react'
 
-import tw, { styled } from 'twin.macro'
-
 import { BadgeAndArrow } from './BadgeAndArrow'
 import { IconAndLabel } from './IconAndLabel'
+import { StyledContainer, StyledItem, StyledUlContainer } from './Menu.styled'
 import { SubItemContainer } from './SubItemContainer'
 
 type ItemProps = {
@@ -171,25 +170,3 @@ export const Menu = () => {
     </StyledContainer>
   )
 }
-
-const StyledContainer = styled.div(() => {
-  return [tw`h-[calc(100%-(var(--navbar-h)*2))] text-sm  select-none`]
-})
-
-const StyledUlContainer = styled.ul(() => {
-  return [tw`list-none px-3`]
-})
-
-const StyledItem = styled.a<{ id: string; lvl: 1 | 2 | 3; activeName: string }>(({ id, lvl, activeName }) => {
-  const activated = activeName === id || activeName.split('.')[0] === id
-
-  return [
-    tw`cursor-pointer rounded-lg flex items-center justify-between h-12 pr-3 mb-1 `,
-    tw`hover:bg-slate-300/20 `,
-    lvl === 1 && tw`pl-4`,
-    lvl === 2 && tw`pl-11`,
-    lvl === 3 && tw`pl-16`,
-    activated ? tw`text-blue-600 font-semibold` : tw`text-slate-500`,
-    activated && lvl === 1 ? tw`bg-blue-200/20` : tw`bg-transparent`,
-  ]
-})
