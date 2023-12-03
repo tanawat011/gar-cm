b0d347800ed7d466bfa12bd657a64a4705649286
Tanawat.P <tanawat.p@appman.co.th>
2023-12-03 20:53:56 +0700

    feat: refactor code a little bit 4

--- src/components/CoreLayout/Sidebar/Content/Menu/Menu.tsx
+++ src/components/CoreLayout/Sidebar/Content/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import type { IconType } from '@/components/Icon'
+import type { ItemProps } from './items'
 
 import type { KeyboardEvent } from 'react'
 import { useRef, useState } from 'react'
@@ -7,61 +7,9 @@ import { BadgeAndArrow } from './BadgeAndArrow'
 import { IconAndLabel } from './IconAndLabel'
 import { StyledContainer, StyledItem, StyledUlContainer } from './Menu.styled'
 import { SubItemContainer } from './SubItemContainer'
-
-type ItemProps = {
-  label: string
-  icon?: IconType
-  id: string
-  link?: string
-  items?: ItemProps[]
-}
+import { items } from './items'
 
 export const Menu = () => {
-  const items: ItemProps[] = [
-    {
-      label: 'Dashboard',
-      icon: 'FaShop',
-      id: 'dashboard',
-    },
-    {
-      label: 'Children',
-      icon: 'FaSignal',
-      id: 'children',
-      items: [
-        {
-          label: 'Children 2',
-          id: 'children2',
-        },
-      ],
-    },
-    {
-      label: 'Level 1',
-      icon: 'FaRegSun',
-      id: 'level1',
-      items: [
-        {
-          label: 'Level 2',
-          id: 'level2',
-        },
-        {
-          label: 'Level 2(2)',
-          id: 'level22',
-          items: [
-            {
-              label: 'Level 3',
-              id: 'level3',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      label: 'Dashboard 2',
-      icon: 'FaShop',
-      id: 'dashboard2',
-    },
-  ]
-
   const activeLink = window.location.pathname
 
   const listRef = useRef<Record<string, HTMLUListElement | null>>({})
