cc540b0e24760601fa796676b35005f007d04dc1
Tanawat.P <tanawat.p@appman.co.th>
2023-12-03 20:30:51 +0700

    feat: new menu item list

---
+++ src/components/CoreLayout/Sidebar/Content/IconAndLabel.tsx
@@ -0,0 +1,19 @@
+import type { IconType } from '@/components/Icon'
+
+import { Icon } from '@/components/Icon'
+
+type IconAndLabelProps = {
+  icon?: IconType
+  label: string
+  isExpand?: boolean
+  isExpandOnHover?: boolean
+}
+
+export const IconAndLabel: React.FC<IconAndLabelProps> = ({ icon, label, isExpand, isExpandOnHover }) => {
+  return (
+    <div className='flex items-center gap-3'>
+      {icon && <Icon name={icon} className='mr-2 w-4 h-4' />}
+      <div className={`truncate ${isExpand ? '' : isExpandOnHover ? '' : 'w-0 h-0 opacity-0'}`}>{label}</div>
+    </div>
+  )
+}
