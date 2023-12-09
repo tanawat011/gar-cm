04581307fbc010573d2be5a44b1f2b7d810cd7b1
Tanawat Pinthongpan <tanawat.works@gmail.com>
2023-12-09 23:10:26 +0700

    feat: apollo, graphql and mongodb

--- src/graphql/user/model.ts
+++ src/graphql/user/model.ts
@@ -5,8 +5,30 @@ import mongoose from 'mongoose'
 const { Schema } = mongoose
 
 const schema = new Schema<Omit<Document, '_id'>>({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: [true, 'All fields are required'],
+    validate: {
+      validator: (v: any) => {
+        console.log('v', v)
+
+        return true
+      },
+      message: (props) => `${props.value} is not a valid email address`,
+    },
+    // validate: {
+    //   as
+    //   isAsync: true,
+    //   validator: (v, cb) => cb(validator.isEmail(v), `${v} is not a valid email address`),
+    // },
+  },
   username: {
     type: String,
+    unique: true,
+    trim: true,
     required: [true, 'All fields are required'],
   },
   password: {
@@ -15,10 +37,6 @@ const schema = new Schema<Omit<Document, '_id'>>({
   },
   firstName: { type: String, required: [true, 'All fields are required'] },
   lastName: {
-    type: String,
-    required: [true, 'All fields are required'],
-  },
-  email: {
     type: String,
     required: [true, 'All fields are required'],
   },
