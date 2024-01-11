1df2198410d9e3ca1f92e944cc419e1aac80a64a
tanawat011 <tanawat.works@gmail.com>
2024-01-11 19:48:15 +0700

    chore(types): :hammer: env.d.ts

---
+++ src/types/env.d.ts
@@ -0,0 +1,20 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    // Define your environment variables here
+    // Example:
+    // API_KEY: string;
+    // DATABASE_URL: string;
+
+    NODE_ENV: 'development' | 'production'
+    PORT: string
+    NEXT_PUBLIC_URL_SERVER_GRAPHQL: string
+    MONGODB_URI: string
+    JWT_SECRET: string
+    AUTH0_SECRET: string
+    AUTH0_BASE_URL: string
+    AUTH0_BASE_URL_IP: string
+    AUTH0_ISSUER_BASE_URL: string
+    AUTH0_CLIENT_ID: string
+    AUTH0_CLIENT_SECRET: string
+  }
+}
